5fec261d180ceff30b2afe47866e765c75639ce6
Marganotvke (CR) <58261562+Marganotvke@users.noreply.github.com>
2022-05-17 23:59:03 +0800

    Create audio-example.fs

---
+++ audio-example.fs
@@ -0,0 +1,19 @@
+// set the color
+#define fg vec3(0,255,255) // foreground
+#define bg vec3(0,0,255) // background
+// set audio buffer average size
+#define sb int(10) // starting audio buffer
+#define eb int(32) // ending audio buffer
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+    vec2 uv = fragCoord.xy / iResolution.xy;
+    vec3 far = fg/255;
+    vec3 close = bg/255;
+    float avg = 0.;
+    for (int i = sb; i < eb; i++){
+        avg += abs(iAudio[i]);
+    }
+    avg /= eb;
+    fragColor = vec4(avg*far+(1.-avg)*close,1.);
+}
